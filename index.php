60d76d9ed4cd84d0740c5cde6afd8dd760823903
angela <angela.delapena06@gmail.com>
2024-04-06 18:35:51 +0800

    changed document title

--- index.php
+++ index.php
@@ -4,7 +4,8 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">
-    <title>Document</title>
+    <link rel="icon" type="image/x-icon" href="images\logo.png">
+    <title>Free Coupon</title>
     <script src="submit.js"></script>
 </head>
 <body>
